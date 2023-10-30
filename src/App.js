6f243c3cb114bc7c2de60321a336f2348ae1580b
GauravChavan18 <chavangaurav597@gmail.com>
2023-10-30 15:48:35 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Registration from "./Pages/Registration";
 import CategoryPage from "./Pages/CategoryPage";
-import HomePage from "./Pages/Home";
+// import HomePage from "./Pages/Home";
 
 function App() {
   return (
