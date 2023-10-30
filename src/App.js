04bfdfd735f5f8666be76eeabb8b036339e9efba
GauravChavan18 <chavangaurav597@gmail.com>
2023-10-30 13:56:48 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Homepage from "./Pages/Homepage";
+
 import Registration from "./Pages/Registration";
 import CategoryPage from "./Pages/CategoryPage";
 
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Homepage />} /> */}
         <Route path="/" element={<Registration />} />
         <Route path="/categorypage" element={<CategoryPage />} />
       </Routes>
