ef651f38adf0a5ac83a9cfe58c2053dd29c842c0
GauravChavan18 <chavangaurav597@gmail.com>
2023-10-30 16:02:12 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Registration from "./Pages/Registration";
 import CategoryPage from "./Pages/CategoryPage";
-// import HomePage from "./Pages/Home";
+import HomePage from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/registrationpage" element={<Registration />} />
         <Route path="/categorypage" element={<CategoryPage />} />
       </Routes>
