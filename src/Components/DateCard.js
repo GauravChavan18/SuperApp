796c2c78639ee4a456c9287b6f24978f1a413655
GauravChavan18 <chavangaurav597@gmail.com>
2023-10-31 00:31:49 +0530

    final changes

--- src/Components/DateCard.js
+++ src/Components/DateCard.js
@@ -1,7 +1,27 @@
-const DateCard = ({ datedata }) => {
-  // const [date, setdate] = useState();
-  // const [time, settime] = useState("");
-  // console.log(datedata);
+import { useCallback, useEffect, useState } from "react";
+
+const DateCard = () => {
+  const [dated, setdated] = useState([]);
+  const getCurrentDate = useCallback(() => {
+    let newDate = new Date();
+    let day = newDate.getDate();
+    let year = newDate.getFullYear();
+    let month = newDate.getMonth();
+    let date = day + "-" + month + "-" + year;
+
+    var hours = newDate.getHours();
+    var minutes = newDate.getMinutes();
+    var ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    var strTime = hours + ":" + minutes + " " + ampm;
+    let arr = [date, strTime];
+    setdated(arr);
+  }, []);
+  useEffect(() => {
+    getCurrentDate();
+  }, [getCurrentDate]);
   return (
     <div
       style={{
@@ -16,8 +36,8 @@ const DateCard = ({ datedata }) => {
         letterSpacing: "1.5px",
       }}
     >
-      {<p style={{ margin: "0" }}>{datedata[0]}</p>}
-      {<p style={{ margin: "0" }}>{datedata[1]}</p>}
+      {<p style={{ margin: "0" }}>{dated[0]}</p>}
+      {<p style={{ margin: "0" }}>{dated[1]}</p>}
     </div>
   );
 };
--- src/Components/WeatherDateCard.js
+++ src/Components/WeatherDateCard.js
@@ -3,8 +3,6 @@ import "./WeatherDateCard.css";
 import DateCard from "./DateCard";
 import WeatherCard from "./WeatherCard";
 const WeatherDateCard = () => {
-  // const [alldata, setalldata] = useState([]);
-  const [datedata, setdatedata] = useState([]);
   const [weatherdata, setweatherdata] = useState(null);
   let url =
     "http://api.weatherapi.com/v1/forecast.json?key=83563ae7d5f14070b8e203915231910&q=mumbai&days=1&aqi=yes&alerts=yes";
@@ -20,32 +18,13 @@ const WeatherDateCard = () => {
     }
   }, [url, setweatherdata]);
 
-  const getCurrentDate = useCallback(() => {
-    let newDate = new Date();
-    let day = newDate.getDate();
-    let year = newDate.getFullYear();
-    let month = newDate.getMonth();
-    let date = day + "-" + month + "-" + year;
-
-    var hours = newDate.getHours();
-    var minutes = newDate.getMinutes();
-    var ampm = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    var strTime = hours + ":" + minutes + " " + ampm;
-    let arr = [date, strTime];
-    setdatedata(...datedata, arr);
-  }, [datedata]);
-
   useEffect(() => {
     fetchData();
-    getCurrentDate();
-  }, [fetchData, getCurrentDate]);
+  }, [fetchData]);
 
   return (
     <div className="WeatherCardDiv">
-      <div className="DateCard">{<DateCard datedata={datedata} />}</div>
+      <div className="DateCard">{<DateCard />}</div>
       <div className="WeatherCard">
         {<WeatherCard weatherdata={weatherdata} />}
       </div>
