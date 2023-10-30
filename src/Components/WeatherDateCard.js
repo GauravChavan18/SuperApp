7e409b569dc838fda80880de86f05e4e400958eb
GauravChavan18 <chavangaurav597@gmail.com>
2023-10-30 15:35:19 +0530

    add

--- src/Components/WeatherDateCard.js
+++ src/Components/WeatherDateCard.js
@@ -1,28 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./WeatherDateCard.css";
 import DateCard from "./DateCard";
 import WeatherCard from "./WeatherCard";
 const WeatherDateCard = () => {
-  const [alldata, setalldata] = useState([]);
+  // const [alldata, setalldata] = useState([]);
   const [datedata, setdatedata] = useState([]);
   const [weatherdata, setweatherdata] = useState(null);
   let url =
     "http://api.weatherapi.com/v1/forecast.json?key=83563ae7d5f14070b8e203915231910&q=mumbai&days=1&aqi=yes&alerts=yes";
 
-  async function fetchdata() {
+  const fetchData = useCallback(async () => {
     try {
       let response = await fetch(url);
       let data = await response.json();
-      setalldata(data);
-      //setdatedata(data.location);
+
       setweatherdata(data.current);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
-  }
+  }, [url, setweatherdata]);
 
-  function getCurrentDate() {
-    //  var ampm = hours >= 12 ? "pm" : "am";
+  const getCurrentDate = useCallback(() => {
     let newDate = new Date();
     let day = newDate.getDate();
     let year = newDate.getFullYear();
@@ -38,11 +36,12 @@ const WeatherDateCard = () => {
     var strTime = hours + ":" + minutes + " " + ampm;
     let arr = [date, strTime];
     setdatedata(...datedata, arr);
-  }
+  }, [datedata]);
+
   useEffect(() => {
-    fetchdata();
+    fetchData();
     getCurrentDate();
-  }, []);
+  }, [fetchData, getCurrentDate]);
 
   return (
     <div className="WeatherCardDiv">
