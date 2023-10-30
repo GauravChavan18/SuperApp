e5273411c45869a572a81599a947db09c1e6a386
GauravChavan18 <chavangaurav597@gmail.com>
2023-10-30 13:03:07 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          add changes
+          add changes to react web
         </a>
       </header>
     </div>
